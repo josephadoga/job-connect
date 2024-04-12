eba5190dfad99eff4ec3449a140cf29bc1ebd922
Navjot Kaur <saininavjot357019@gmail.com>
2024-04-12 13:35:37 -0500

    Started JavaScript

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,4 +1,16 @@
 'use strict';
 
-import { listen, select } from "./utils.js";
+import { listen, select } from './utils.js';
 
+const login = select('.login');
+const signup = select('.signup');
+const userEmail = select('.email');
+const userPassword = select('.password');
+let invalid = select('.invalid');
+
+signup.disabled = true;
+
+
+const loginCredentials = [{ email: 'user@email.com', password: 'userpass' }];
+localStorage.setItem('loginCredentials', JSON.stringify(loginCredentials));
+const loginValues = JSON.parse(localStorage.getItem('loginCredentials'));
